f7b91d4989cd130f6d7d72e9188e7ee255d5d051
astadnicka <aleksandra.stadnicka@outlook.com>
2025-06-12 22:15:50 +0200

    more networks

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-
+//   output: 'export',
+  images: { unoptimized: true }
 };
-
 export default nextConfig;
