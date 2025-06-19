a9714647ca92247e69b9a61dca224653f4e4c88c
astadnicka <aleksandra.stadnicka@outlook.com>
2025-06-19 11:59:14 +0200

    działa:)

--- backend-posts/config.py
+++ backend-posts/config.py
@@ -1,7 +1,9 @@
-MYSQL_HOST = 'mysql'
-MYSQL_USER = 'root'
-# MYSQL_PASSWORD = 'rootpassword'
-MYSQL_DB = 'quizdb'
+import os
+
+MYSQL_HOST = os.getenv('MYSQL_HOST', 'mysql')
+MYSQL_USER = os.getenv('MYSQL_USER', 'root')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'rootpassword')
+MYSQL_DB = os.getenv('MYSQL_DATABASE', 'quizdb')
 MYSQL_CHARSET = 'utf8mb4'
 MYSQL_INIT_COMMAND = 'SET NAMES utf8mb4 COLLATE utf8mb4_polish_ci'
 
